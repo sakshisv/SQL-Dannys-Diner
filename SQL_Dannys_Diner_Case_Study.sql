a79d1cfd938e2300be9f41ee9e3d4c2b4f4b4e3a
Sakshi <sakshi0317sv@gmail.com>
2023-05-07 02:54:05 +0530

    Third Commit

--- SQL_Dannys_Diner_Case_Study.sql
+++ SQL_Dannys_Diner_Case_Study.sql
@@ -6,12 +6,13 @@ select * from sales
 
 -- Q1. What is the total amount each customer spent at the restaurant?
 
-select a.customer_id, sum(b.price) Total_Amount from sales a
+select a.customer_id, sum(b.price) total_amount from sales a
 left join menu b
 on a.product_id = b.product_id
 group by a.customer_id
 
+--Q2. How many days has each customer visited the restaurant?
 
-
-
+select customer_id, count(distinct(order_date)) as visit_count from sales
+group by customer_id
 
