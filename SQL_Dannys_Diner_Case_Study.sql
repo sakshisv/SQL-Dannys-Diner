5551d018d266b015cee5fd2871437185c018ea87
Sakshi <sakshi0317sv@gmail.com>
2023-05-07 02:44:54 +0530

    Second Commit

--- SQL_Dannys_Diner_Case_Study.sql
+++ SQL_Dannys_Diner_Case_Study.sql
@@ -3,3 +3,15 @@ use Dannys_Diner
 select * from members
 select * from menu
 select * from sales
+
+-- Q1. What is the total amount each customer spent at the restaurant?
+
+select a.customer_id, sum(b.price) Total_Amount from sales a
+left join menu b
+on a.product_id = b.product_id
+group by a.customer_id
+
+
+
+
+
